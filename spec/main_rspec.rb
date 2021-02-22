71ca4f9a28e3886f52ca91238e2e4961ac5e7fd2
nganifaith <nganifaith@gmail.com>
2021-02-22 15:02:42 +0100

    Add test for flip user and sign validation.

--- spec/main_rspec.rb
+++ spec/main_rspec.rb
@@ -7,10 +7,31 @@ require_relative 'main'
 include Player # rubocop:disable Style/MixinUsage
 include Settings # rubocop:disable Style/MixinUsage
 
+describe "#Player.sign_validate" do
+  it 'returns true' do
+    expect(Player.sign_validate('x')).to eql(true)
+    expect(Player.sign_validate('x')).to eql(true)
+  end
+end
+
+describe "#players" do
+  it 'prints player next move and returns sign' do
+    expect(players('x')).to eql('x')
+    expect(players('o')).to eql('o')
+  end
+end
+
+describe "#Player.move_validate" do
+  it "validates the move" do
+    expect(Player.move_validate('1')).to eql(true)
+    expect(Player.move_validate('9')).to eql(true)
+    expect(Player.move_validate('10')).to eql(false)
+  end
+end
+
 describe 'Settings.draw_board' do
   it 'return the string in order to print the board' do
-    expect(Settings.draw_board([1, 2, 3, 4, 5, 6, 7, 8,
-                                9])).to eql("\n\n\n   |   |   \n 1 | 2 | 3 \n   |   |   \n---+---+---\n   |   |   \n 4 | 5 | 6 \n   |   |   \n --+---+-- \n   |   |   \n 7 | 8 | 9 \n   |   |   \n\n\n")
+    expect(Settings.draw_board([1, 2, 3, 4, 5, 6, 7, 8, 9])).to eql("\n\n\n   |   |   \n 1 | 2 | 3 \n   |   |   \n---+---+---\n   |   |   \n 4 | 5 | 6 \n   |   |   \n --+---+-- \n   |   |   \n 7 | 8 | 9 \n   |   |   \n\n\n")
   end
 end
 
@@ -21,10 +42,10 @@ describe 'Player.index_taken' do
   end
 end
 
-# describe Player do
-#   describe "#sign_validate" do
-#     it "return true or false" do
-#       sign_validate('x').to eql(true)
-#     end
-#   end
-# end
+describe 'Player.flip_user' do
+  it 'return O if player is X or X if player is O' do
+    expect(Player.flip_user('x')).to eql('o')
+    expect(Player.flip_user('o')).to eql('x')
+  end
+end
+
