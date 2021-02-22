242b06914cd3ad49646aae99cc0f82bc6a600230
vidhishweta01 <vidhishweta01@gmail.com>
2021-02-22 19:48:32 +0530

    fixed linter errors

--- spec/main_rspec.rb
+++ spec/main_rspec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative '../lib/settings'
 require_relative '../lib/logic'
 require_relative 'main'
@@ -7,22 +5,22 @@ require_relative 'main'
 include Player # rubocop:disable Style/MixinUsage
 include Settings # rubocop:disable Style/MixinUsage
 
-describe "#Player.sign_validate" do
+describe '#Player.sign_validate' do
   it 'returns true' do
     expect(Player.sign_validate('x')).to eql(true)
-    expect(Player.sign_validate('x')).to eql(true)
+    expect(Player.sign_validate('o')).to eql(true)
   end
 end
 
-describe "#players" do
+describe '#players' do
   it 'prints player next move and returns sign' do
     expect(players('x')).to eql('x')
     expect(players('o')).to eql('o')
   end
 end
 
-describe "#Player.move_validate" do
-  it "validates the move" do
+describe '#Player.move_validate' do
+  it 'validates the move' do
     expect(Player.move_validate('1')).to eql(true)
     expect(Player.move_validate('9')).to eql(true)
     expect(Player.move_validate('10')).to eql(false)
@@ -31,7 +29,8 @@ end
 
 describe 'Settings.draw_board' do
   it 'return the string in order to print the board' do
-    expect(Settings.draw_board([1, 2, 3, 4, 5, 6, 7, 8, 9])).to eql("\n\n\n   |   |   \n 1 | 2 | 3 \n   |   |   \n---+---+---\n   |   |   \n 4 | 5 | 6 \n   |   |   \n --+---+-- \n   |   |   \n 7 | 8 | 9 \n   |   |   \n\n\n")
+    expect(Settings.draw_board([1, 2, 3, 4, 5, 6, 7, 8,
+                                9])).to eql("\n\n\n   |   |   \n 1 | 2 | 3 \n   |   |   \n---+---+---\n   |   |   \n 4 | 5 | 6 \n   |   |   \n --+---+-- \n   |   |   \n 7 | 8 | 9 \n   |   |   \n\n\n")
   end
 end
 
@@ -48,4 +47,3 @@ describe 'Player.flip_user' do
     expect(Player.flip_user('o')).to eql('x')
   end
 end
-
