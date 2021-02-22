90d09c7e31c62e999210fe3a5f6a06150d84498c
vidhishweta01 <vidhishweta01@gmail.com>
2021-02-22 20:53:55 +0530

    fixed linters

--- spec/main_rspec.rb
+++ spec/main_rspec.rb
@@ -4,14 +4,14 @@ require_relative 'main'
 
 include Player # rubocop:disable Style/MixinUsage
 include Settings # rubocop:disable Style/MixinUsage
-array = ['x', 'x', 'x',3 , 4, 'o', 'o', 7, 8]
+array = ['x', 'x', 'x', 3, 4, 'o', 'o', 7, 8]
 array1 = [0, 1, 2, 'x', 'x', 'x', 'o', 7, 'o']
 array2 = [0, 1, 2, 'o', 4, 'o', 'x', 'x', 'x']
-array3 = ['x', 1, 2, 'x' , 4, 'o', 'x', 7, 8]
-array4 = ['o', 'x', 'x',3 , 'x', 5, 'o', 'x', 8]
-array5 = ['o', 'o', 'x',3 , 4, 'x', 'o',7, 'x']
-array6 = ['x', 1,'o', 3,'x','o', 6, 7,'x']
-array7 =[1,2,'x', 4, 'x', 6, 'x', 8, 9]
+array3 = ['x', 1, 2, 'x', 4, 'o', 'x', 7, 8]
+array4 = ['o', 'x', 'x', 3, 'x', 5, 'o', 'x', 8]
+array5 = ['o', 'o', 'x', 3, 4, 'x', 'o', 7, 'x']
+array6 = ['x', 1, 'o', 3, 'x', 'o', 6, 7, 'x']
+array7 = [1, 2, 'x', 4, 'x', 6, 'x', 8, 9]
 array8 = ['o', 'o', 'o', 4, 5, 6, 7, 8, 9]
 array9 = [1, 2, 3, 'o', 'o', 'o', 7, 8, 9]
 array10 = [1, 2, 3, 4, 5, 6, 'o', 'o', 'o']
@@ -20,7 +20,6 @@ array12 = [1, 'o', 3, 4, 'o', 6, 7, 'o', 9]
 array13 = [1, 2, 'o', 4, 5, 'o', 7, 8, 'o']
 array14 = ['o', 2, 3, 4, 'o', 6, 7, 8, 'o']
 array15 = [1, 2, 'o', 4, 'o', 6, 'o', 8, 9]
-
 
 describe '#Player.sign_validate' do
   it 'returns true' do
@@ -67,21 +66,21 @@ end
 
 describe 'Player.wins' do
   it 'return true for win cases' do
-    expect(Player.wins(array,'x')).to eql(true)
-    expect(Player.wins(array1,'x')).to eql(true)
-    expect(Player.wins(array2,'x')).to eql(true)
-    expect(Player.wins(array3,'x')).to eql(true)
-    expect(Player.wins(array4,'x')).to eql(true)
-    expect(Player.wins(array5,'x')).to eql(true)
-    expect(Player.wins(array6,'x')).to eql(true)
-    expect(Player.wins(array7,'x')).to eql(true)
-    expect(Player.wins(array8,'o')).to eql(true)
-    expect(Player.wins(array9,'o')).to eql(true)
-    expect(Player.wins(array10,'o')).to eql(true)
-    expect(Player.wins(array11,'o')).to eql(true)
-    expect(Player.wins(array12,'o')).to eql(true)
-    expect(Player.wins(array13,'o')).to eql(true)
-    expect(Player.wins(array14,'o')).to eql(true)
-    expect(Player.wins(array15,'o')).to eql(true)
+    expect(Player.wins(array, 'x')).to eql(true)
+    expect(Player.wins(array1, 'x')).to eql(true)
+    expect(Player.wins(array2, 'x')).to eql(true)
+    expect(Player.wins(array3, 'x')).to eql(true)
+    expect(Player.wins(array4, 'x')).to eql(true)
+    expect(Player.wins(array5, 'x')).to eql(true)
+    expect(Player.wins(array6, 'x')).to eql(true)
+    expect(Player.wins(array7, 'x')).to eql(true)
+    expect(Player.wins(array8, 'o')).to eql(true)
+    expect(Player.wins(array9, 'o')).to eql(true)
+    expect(Player.wins(array10, 'o')).to eql(true)
+    expect(Player.wins(array11, 'o')).to eql(true)
+    expect(Player.wins(array12, 'o')).to eql(true)
+    expect(Player.wins(array13, 'o')).to eql(true)
+    expect(Player.wins(array14, 'o')).to eql(true)
+    expect(Player.wins(array15, 'o')).to eql(true)
   end
 end
